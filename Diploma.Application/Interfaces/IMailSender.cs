4ef08abcb549fa22b70f9e3d1a59c512d630f667
KorolevYA <KorolevYA@kamaz.ru>
2023-05-15 10:29:49 +0300

    Add EmailSender

---
+++ Diploma.Application/Interfaces/IMailSender.cs
@@ -0,0 +1,14 @@
+﻿namespace Diploma.Application.Interfaces
+{
+    public interface IMailSender
+    {
+        /// <summary>
+        /// Отправка сообщения на почту
+        /// </summary>
+        /// <param name="emailTo">Адресат</param>
+        /// <param name="subject">Тема</param>
+        /// <param name="body">Сообщение</param>
+        /// <returns></returns>
+        Task SendEmailAsync(string emailTo, string subject, string body);
+    }
+}
